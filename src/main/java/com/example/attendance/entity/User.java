7574ccc4d4459645260ca44a8bebfd89c2ff1942
Thushari_Herath <thushariherath716@gmail.com>
2026-01-07 09:40:24 +0530

    ADD THE QR CODE

--- src/main/java/com/example/attendance/entity/User.java
+++ src/main/java/com/example/attendance/entity/User.java
@@ -2,7 +2,6 @@ package com.example.attendance.entity;
 
 import jakarta.persistence.*;
 
-
 @Entity
 public class User {
 
@@ -13,74 +12,94 @@ public class User {
     private String name;
     private String company;
     private String department;
+    private String epfNo;
+    private String qrCode;
     private int tableNo = 0;
     private boolean attend = Boolean.FALSE;
 
     public User() {}
 
-    public User(int id, String name, String company, String department, int tableNo, boolean attend, String status) {
+    public User(int id, String status, String name, String company, String department, String epfNo, String qrCode, int tableNo, boolean attend) {
         this.id = id;
+        this.status = status;
         this.name = name;
         this.company = company;
         this.department = department;
+        this.epfNo = epfNo;
+        this.qrCode = qrCode;
         this.tableNo = tableNo;
         this.attend = attend;
-        this.status = status;
     }
 
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getStatus() {
         return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getCompany() {
-        return company;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public int getTableNo() {
-        return tableNo;
-    }
-
-    public boolean isAttend() {
-        return attend;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getCompany() {
+        return company;
     }
 
     public void setCompany(String company) {
         this.company = company;
     }
 
+    public String getDepartment() {
+        return department;
+    }
+
     public void setDepartment(String department) {
         this.department = department;
+    }
+
+    public String getEpfNo() {
+        return epfNo;
+    }
+
+    public void setEpfNo(String epfNo) {
+        this.epfNo = epfNo;
+    }
+
+    public String getQrCode() {
+        return qrCode;
+    }
+
+    public void setQrCode(String qrCode) {
+        this.qrCode = qrCode;
+    }
+
+    public int getTableNo() {
+        return tableNo;
     }
 
     public void setTableNo(int tableNo) {
         this.tableNo = tableNo;
     }
 
-    public void setAttend(boolean attend) {
-        this.attend = attend;
+    public boolean isAttend() {
+        return attend;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
+    public void setAttend(boolean attend) {
+        this.attend = attend;
     }
 }
