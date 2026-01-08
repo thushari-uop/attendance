fa2a57dc16561f7c5a2940c80a2d270c5686c5fa
Dinesh Lahiru <dnshlahiru@gmail.com>
2026-01-08 10:17:55 +0530

    CORS

--- src/main/java/com/example/attendance/controller/Api.java
+++ src/main/java/com/example/attendance/controller/Api.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS})
 public class Api {
 
     @Autowired
